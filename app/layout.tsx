7f1029cf8ff4251a813212c02b02e17bc7172ecb
NghiepDev <me@nghiep.dev>
2025-04-22 14:44:24 +0700

    Update deps

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type {Metadata} from 'next';
-import {Geist, Geist_Mono} from 'next/font/google';
 import './globals.css';
 import {cn} from '@/lib/cn';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://tiemtrabaco.com'),
@@ -27,11 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="vi">
-      <body
-        className={cn(geistSans.variable, geistMono.variable, 'antialiased')}
-      >
-        {children}
-      </body>
+      <body className={cn('antialiased')}>{children}</body>
     </html>
   );
 }
